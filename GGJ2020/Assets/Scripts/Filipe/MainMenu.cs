8763855e28faf5423fa418c0b79ef3ab17b064de
Filipe <fantunes.dev@gmail.com>
2020-02-02 01:26:50 -0500

    Main Menu

--- GGJ2020/Assets/Scripts/Filipe/MainMenu.cs
+++ GGJ2020/Assets/Scripts/Filipe/MainMenu.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public GameObject soundMenu;
+
+   private void Start()
+   {
+      soundMenu.SetActive(false);
+   }
+
+   public void LoadGame()
+   {
+      SceneManager.LoadScene("GameScene 1");
+   }
+
+   public void OpenSoundSettings()
+   {
+      soundMenu.SetActive(true);
+   }
+   
+   public void CloseSoundSettings()
+   {
+      soundMenu.SetActive(false);
+   }
+   public void ExitGame()
+   {
+      Application.Quit();
+   }
 }
